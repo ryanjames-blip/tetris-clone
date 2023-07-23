78fabe800af38d3cc5d9923dc6cf591c6d04bfe4
Ryan <ryandenbleyker@gmail.com>
2023-07-23 16:17:42 -0400

    Complete basic setup

--- script.js
+++ script.js
@@ -5,4 +5,32 @@ const ctx = canvas.getContext('2d');
 canvas.width = 500;
 canvas.height = 800;
 
+let score = 0;
+let gameFrame = 0;
+
+// Arrow keys
+addEventListener('keydown', moveBlock);
+
+// Blocks
+class Block {
+    constructor() {
+        this.x = canvas.width / 2;
+        this.y = canvas.height / 2;
+        this.radius = 50;
+        this.angle = 0;
+        this.frameX = 0;
+        this.frameY = 0;
+        this.spriteWidth = 498;
+        this.spriteHeight = 327;
+    }
+    update() {
+        
+    }
+}
+
+function moveBlock(event) {
+    console.log(event.code);
+}
+
+// Animation loop
 
